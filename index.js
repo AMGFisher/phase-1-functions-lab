9afefcc5e0616ceca209b477406598627bbe25e0
Adam Fisher <amgfisher90@gmail.com>
2022-11-29 19:08:44 -0500

    commit tree 9e89c2f0ec8546fb9ec844a446a47f668ea79ef6

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+const hq = 42
+
+
+function distanceFromHqInBlocks(userStreet) {
+return Math.abs(userStreet - 42)
+}
+
+
+
+function distanceFromHqInFeet(userStreet) {
+distanceFromHqInBlocks(userStreet);
+return distanceFromHqInBlocks(userStreet) * 264;
+}
+
+
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start - destination) * 264)
+}
+
+
+
+function calculatesFarePrice(start, destination) {
+    let distance = Math.abs((start - destination) * 264)
+    if (distance <= 400) {
+        return 0
+    }
+    if (distance > 2500) {
+        return "cannot travel that far"
+    }
+    if (distance > 2000) {
+        return 25
+    }
+    else if (distance > 400) {
+        return (distance - 400) * .02
+    }
+
+}
